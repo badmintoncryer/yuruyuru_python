d186a1cb12e674fa588ef037954913b735d45357
kazuho <malaysia.cryer@gmail.com>
2021-05-15 14:31:06 +0900

    [fixed] bugfix

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from time import sleep
 import machine
 
+BOOLIAN_FLAG = False
+
 # ESP32 - Pin assignment
 pin14 = machine.Pin(14, machine.Pin.OUT)
-high_low_flag = 1
-pin14.value(high_low_flag)
+if (BOOLIAN_FLAG):
+    high_low_flag = True
+else:
+    high_low_flag = 0x91
 
 while True:
-    pin14.value(~high_low_flag)
-    sleep(5)
+    if (BOOLIAN_FLAG):
+        high_low_flag = not high_low_flag
+    else:
+        high_low_flag = ~high_low_flag & 0x01
+
+    pin14.value(high_low_flag)
+    sleep(1)
