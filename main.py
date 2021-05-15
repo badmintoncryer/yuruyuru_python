b742597aa7de4652baebb8649bd9df7852eb93b4
kazuho <malaysia.cryer@gmail.com>
2021-05-15 15:00:07 +0900

    [fixed] bugfix

--- main.py
+++ main.py
@@ -1,30 +1,32 @@
 from time import sleep
 import machine
-import driver.BME280
+import BME280
 
-BOOLIAN_FLAG = False
+BOOLIAN_FLAG = True
 
 # ESP32 - Pin assignment
 i2c = machine.I2C(scl=machine.Pin(22), sda=machine.Pin(21), freq=10000)
 pin14 = machine.Pin(14, machine.Pin.OUT)
+
 if (BOOLIAN_FLAG):
     high_low_flag = True
 else:
     high_low_flag = 0x01
 
 while True:
+    if (BOOLIAN_FLAG):
+        high_low_flag = not high_low_flag
+    else:
+        high_low_flag = ~high_low_flag & 0x01
+    pin14.value(high_low_flag)
+
     bme = BME280.BME280(i2c=i2c)
     temp = bme.temperature
     hum = bme.humidity
     pres = bme.pressure
 
-    # uncomment for temperature in Fahrenheit
-    #temp = (bme.read_temperature()/100) * (9/5) + 32
-    #temp = str(round(temp, 2)) + 'F'
-
     print('Temperature: ', temp)
     print('Humidity: ', hum)
     print('Pressure: ', pres)
 
-    pin14.value(~high_low_flag)
-    sleep(5)
+    sleep(1)
